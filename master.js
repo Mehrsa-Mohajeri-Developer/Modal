5109c16d53270adc244f349e6837c0032bcf035c
Mehrsa Mohajeri <145048780+Mehrsa-Mohajeri-Developer@users.noreply.github.com>
2023-12-11 18:35:26 +0330

    Add files via upload

---
+++ master.js
@@ -0,0 +1,11 @@
+function button(){
+    document.getElementById("box1").style.display = "none"
+    document.getElementById("box2").style.display = "flex"
+}
+function exit(){
+    document.getElementById("box1").style.display = "flex"
+    document.getElementById("box2").style.display = "none"
+}
+if ('document.getElementById("box1").style.display = "none"'){
+ exitbody()
+}
